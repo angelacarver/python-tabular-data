759e1c34131510efcf069545dbc595e6027467b0
Angela Carver <acc0061@venus.auburn.edu>
2021-03-22 16:23:24 -0500

    added the full commands for plotting, etc. Function still unfinished

--- iris_regression.py
+++ iris_regression.py
@@ -8,7 +8,7 @@ import panda as pd
 import matplotlib.pyplot as plt
 from scipy import stats
 
-def regress(species):
+def regress_and_plot(species):
     """
     Gets the lines of data out of iris.csv based on the species entered.
     
@@ -18,7 +18,7 @@ def regress(species):
             The name of the species for which you want data.
     Returns
     -------
-    regression slope and intercept or None
+    regression slope, intercept, and .png of the plot or None
             A linear regression of sepal length vs petal length data from the rows of 
             data in iris.csv which pertain to the species entered or None if the species 
             entered is not in iris.csv.
@@ -30,8 +30,14 @@ def regress(species):
     regression = stats.linregress(x, y)
     slope = regression.slope
     intercept = regression.intercept
+    plt.scatter(x, y, label = 'Data')
+    plt.plot(x, slope * x + intercept, color = "orange", label = 'Fitted line')
+    plt.xlabel("Petal length (cm)")
+    plt.ylabel("Sepal length (cm)")
+    plt.legend()
+    plt.savefig("petal_v_sepal_length_regress.png")
     #if dataframespecies isn't empty
-    return slope, intercept
+    #return slope, intercept, plot
 
 
 

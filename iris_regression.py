f33ede7b4905fd600300bc5db0d8e71e658dbb06
Angela Carver <acc0061@venus.auburn.edu>
2021-03-24 13:04:20 -0500

    Finished rough outline of function, including adding if/else. Called function for troubleshooting.

--- iris_regression.py
+++ iris_regression.py
@@ -4,10 +4,12 @@
 
 "A script which performs and plots linear regressions of petal length vs. sepal length for the three types of irises in iris.csv."
 
-import panda as pd
+import pandas as pd
 import matplotlib.pyplot as plt
 from scipy import stats
 
+intstr = 'str'
+slpstr = 'str'
 def regress_and_plot(species):
     """
     Gets the lines of data out of iris.csv based on the species entered.
@@ -15,7 +17,8 @@ def regress_and_plot(species):
     Parameters
     ----------
     species: str
-            The name of the species for which you want data.
+            The name of the species for which you want data separated by underscores and surrounded by quotation marks (""). 
+            Example: "Iris_virginica"
     Returns
     -------
     regression slope, intercept, and .png of the plot or None
@@ -23,21 +26,34 @@ def regress_and_plot(species):
             data in iris.csv which pertain to the species entered or None if the species 
             entered is not in iris.csv.
     """
-    dataframe = read.csv("iris.csv")
+    dataframe = read_csv("iris.csv")
     dataframespecies = dataframe[dataframe.species == species]
-    x = dataframespecies.petal_length_cm
-    y = dataframespecies.sepal_length_cm
-    regression = stats.linregress(x, y)
-    slope = regression.slope
-    intercept = regression.intercept
-    plt.scatter(x, y, label = 'Data')
-    plt.plot(x, slope * x + intercept, color = "orange", label = 'Fitted line')
-    plt.xlabel("Petal length (cm)")
-    plt.ylabel("Sepal length (cm)")
-    plt.legend()
-    plt.savefig("petal_v_sepal_length_regress.png")
+    if dataframespecies == df.empty:
+        print('Species not in iris.csv.')
+    else:
+        x = dataframespecies.petal_length_cm
+        y = dataframespecies.sepal_length_cm
+        regression = stats.linregress(x, y)
+        slope = regression.slope
+        intercept = regression.intercept
+        plt.scatter(x, y, label = 'Data')
+        plt.plot(x, slope * x + intercept, color = "orange", label = 'Fitted line')
+        plt.xlabel("Petal length (cm)")
+        plt.ylabel("Sepal length (cm)")
+        plt.legend()
+        plt.savefig("petal_v_sepal_length_regress.png")
+        intstr = 'intercept:'
+        slpstr = 'slope:'
+        return [intstr, intercept, slpstr, slope]
+        print('Regression saved as petal_v_sepal_length_regress.png.')
     #if dataframespecies isn't empty
     #return slope, intercept, plot
+
+regress_and_plot("Iris_virginica")
+
+#regress_and_plot(Iris_versicolor)
+
+#regress_and_plot(Iris_setosa)
 
 
 
